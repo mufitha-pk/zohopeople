b91a3d3206bfb78e7a3f9057605159c5cf930ac4
mufitha pk <mufithajunaid@gmail.com>
2026-01-22 12:29:21 +0530

    Updated add_cmpny_details page

--- add_cmpny_details.php
+++ add_cmpny_details.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-Type: application/json");
 include "db.php"; // Database connection
-print"hi";
+
 
 // 🔹 Get POST data (JSON)
 $data = json_decode(file_get_contents("php://input"), true);
